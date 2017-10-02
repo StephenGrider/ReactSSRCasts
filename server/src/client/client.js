eaa0cedd717ed05699d12544ec4820f81cc1719e
stephengrider <ste.grider@gmail.com>
2017-10-02 13:29:55 -0700

    added client webpack config

---
+++ server/src/client/client.js
@@ -0,0 +1,3 @@
+// Startup point for the client side application
+
+console.log('Hi there!');
---
+++ server/webpack.client.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  // Tell webpack the root file of our
+  // server application
+  entry: './src/client/client.js',
+
+  // Tell webpack where to put the output file
+  // that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+
+  // Tell webpack to run babel on every file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: { browsers: ['last 2 versions'] } }]
+          ]
+        }
+      }
+    ]
+  }
+};
