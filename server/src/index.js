863b6dd346461253a5b6692372bd4b56601c10ea
stephengrider <ste.grider@gmail.com>
2017-10-02 10:44:36 -0700

    added rendertostring

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
 const app = express();
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+
+  res.send(content);
+});
 
 app.listen(3000, () => {
   console.log('Listening on prot 3000');
