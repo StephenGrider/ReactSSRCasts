750dafcffeedde39b4f75875b6179fb2f05eaa3e
stephengrider <ste.grider@gmail.com>
2017-10-02 11:24:55 -0700

    jsx on server

---
+++ server/webpack.server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  // Inform webpack that we're building a bundle
+  // for nodeJS, rather than for the browser
+  target: 'node',
+
+  // Tell webpack the root file of our
+  // server application
+  entry: './src/index.js',
+
+  // Tell webpack where to put the output file
+  // that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  // Tell webpack to run babel on every file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: { browsers: ['last 2 versions'] } }]
+          ]
+        }
+      }
+    ]
+  }
+};
