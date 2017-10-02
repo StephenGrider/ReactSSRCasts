351984caeb5b99715033daf198084c459bba9345
stephengrider <ste.grider@gmail.com>
2017-10-02 13:40:57 -0700

    public dir

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -6985,10 +6985,13 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 
+app.use(_express2.default.static('public'));
 app.get('/', function (req, res) {
   var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
-  res.send(content);
+  var html = '\n    <html>\n      <head></head>\n      <body>\n        <div>' + content + '</div>\n        <script src="bundle.js"></script>\n      </body>\n    </html>\n  ';
+
+  res.send(html);
 });
 
 app.listen(3000, function () {
--- server/src/index.js
+++ server/src/index.js
@@ -5,10 +5,21 @@ import Home from './client/components/Home';
 
 const app = express();
 
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   const content = renderToString(<Home />);
 
-  res.send(content);
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div>${content}</div>
+        <script src="bundle.js"></script>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
 });
 
 app.listen(3000, () => {
