0a7528fa221a5a4cc318db69f3cff1d87e614a68
stephengrider <ste.grider@gmail.com>
2017-10-05 10:41:49 -0700

    matchroutes

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -88,6 +88,12 @@ var _express = __webpack_require__(3);
 
 var _express2 = _interopRequireDefault(_express);
 
+var _reactRouterConfig = __webpack_require__(18);
+
+var _Routes = __webpack_require__(6);
+
+var _Routes2 = _interopRequireDefault(_Routes);
+
 var _renderer = __webpack_require__(4);
 
 var _renderer2 = _interopRequireDefault(_renderer);
@@ -104,8 +110,7 @@ app.use(_express2.default.static('public'));
 app.get('*', function (req, res) {
   var store = (0, _createStore2.default)();
 
-  // Some logic to initialize
-  // and load data into the store
+  console.log((0, _reactRouterConfig.matchRoutes)(_Routes2.default, req.path));
 
   res.send((0, _renderer2.default)(req, store));
 });
--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
@@ -9,8 +11,7 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
   const store = createStore();
 
-  // Some logic to initialize
-  // and load data into the store
+  console.log(matchRoutes(Routes, req.path));
 
   res.send(renderer(req, store));
 });
