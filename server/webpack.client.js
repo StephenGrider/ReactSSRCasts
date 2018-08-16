a1a0aac42ce515020c3a0a24fb7ab1e9be17ec7d
stephen grider <ste.grider@gmail.com>
2018-08-16 08:53:33 -0600

    prod configs

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,6 +1,8 @@
+const webpack = require('webpack');
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const Uglify = require('uglifyjs-webpack-plugin');
 
 const config = {
   // Tell webpack the root file of our
@@ -12,7 +14,18 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: process.env.NODE_ENV
+      }
+    })
+  ]
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(new Uglify());
+}
 
 module.exports = merge(baseConfig, config);
