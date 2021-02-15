eee0d9e3342aacec8725d83935efd84a02a23116
Artem Klymov <artem@mediotype.com>
2021-02-16 00:30:05 +0200

    SSR-5: Remove rewritten DataResolved file and usages

--- src/helpers/createStore.js
+++ src/helpers/createStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../client/reducers';
-import DataResolver from '../services/DataResolver';
+import { DataResolver } from '@reactmono/core';
 
 export default (req) => {
     const dataResolver = new DataResolver(req);
