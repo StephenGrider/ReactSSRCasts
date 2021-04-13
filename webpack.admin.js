6df65fa7790d1d36f47e6af13b8edaae6625ebea
Artem Klymov <artem@mediotype.com>
2021-04-13 23:45:53 +0300

    SSR-32 Improve package.json commands to get rid of long tail line dev commands

---
+++ webpack.admin.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+
+const config = {
+    // Tell webpack the root file of our
+    // client application
+    entry: './src/admin/admin.js',
+
+    // Tell webpack where to put the output file
+    // that is generated
+    output: {
+        filename: 'admin-bundle.js',
+        path: path.resolve(__dirname, 'public')
+    }
+};
+
+module.exports = merge(baseConfig, config);
