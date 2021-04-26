ac8a720a8e2a5d506b1b889559fd44389b96c263
Artem Klymov <artem@mediotype.com>
2021-04-27 00:23:33 +0300

    SSR-35 Add Alerts component flyout for messages: Fix missing client alert reducer

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { alert } from '@reactmono/components';
 import userReducer from './userReducer';
 import usersReducer from './usersReducer';
 import authReducer from './authReducer';
 import adminsReducer from './adminsReducer';
 
 export default combineReducers({
+    alert: alert.reducer,
     user: userReducer,
     users: usersReducer,
     auth: authReducer,
