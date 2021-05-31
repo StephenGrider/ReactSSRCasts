eb22e070a6b8974dffe6a4a3e55ea295af69016e
Artem Klymov <artem@mediotype.com>
2021-05-31 23:35:22 +0300

    SSR-49 Add sort order to routes

--- app/admin/module/page-not-found/route.js
+++ app/admin/module/page-not-found/route.js
@@ -2,6 +2,7 @@ import loadable from '@loadable/component';
 
 export default [
     {
-        component: loadable(() => import(`./NotFoundPage`))
+        component: loadable(() => import(`./NotFoundPage`)),
+        sortOrder: 1000
     }
 ];
--- app/client/bootstrap/routeProcessor.js
+++ app/client/bootstrap/routeProcessor.js
@@ -3,6 +3,8 @@ import loadable from '@loadable/component';
 import modules from '~app/util/client/getModules';
 import loaderGenerator from '~app/util/base/loaderGenerator';
 
+const defaultSortOrder = 999;
+
 /** Collect loaders for routes from modules */
 let loaders = Object.keys(modules).reduce((collectedLoader, module) => {
     let { loader } = modules[module];
@@ -48,6 +50,17 @@ let routes = Object.keys(modules).reduce((accumulatedConfigs, module) => {
         return [...accumulatedConfigs, route];
     }
 }, []);
+
+routes = routes.sort((routeA, routeB) => {
+    let orderA = routeA.sortOrder ? routeA.sortOrder : defaultSortOrder;
+    let orderB = routeB.sortOrder ? routeB.sortOrder : defaultSortOrder;
+
+    return orderA < orderB
+        ? -1
+        : orderA > orderB
+            ? 1
+            : 0;
+});
 
 /** Routes configuration */
 export default () => ([
